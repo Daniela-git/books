514756b82758e6343605a8b16d29b24dc7430c7e
Daniela-git <dhiguitaa17@gmail.com>
2024-01-09 16:21:25 -0500

    Revert "send notification if fails"
    This reverts commit 4b48101d6af05e42b8efac4c99aad36500b4fe85.

--- axios/sendError.js
+++
@@ -1,4 +0,0 @@
-const sendNotification = require('./sendNotification');
-sendNotification(
-  'Books failed today try manual trigger https://github.com/Daniela-git/books/actions/workflows/schedule-run.yml'
-);
