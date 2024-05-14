f321282da91581f30c605c8a5ba7e21382de5082
Daniela-git <dhiguitaa17@gmail.com>
2024-05-14 09:21:45 -0500

    new logic

--- axios/sendNewLowest.js
+++ axios/sendNewLowest.js
@@ -3,17 +3,16 @@ import { join } from 'path';
 import sendNotification from './sendNotifications.js';
 
 function sendNewLowestEmail() {
-  const lowest = JSON.parse(readFileSync('./newLowest.json'));
-  let text = '';
-  for (const book of lowest) {
-    text = `${text}\n${book.title}: $${book.price}`;
+  try {
+    const lowest = JSON.parse(readFileSync('./newLowest.json'));
+    let text = '';
+    for (const book of lowest) {
+      text = `${text}\n${book.title}: $${book.price}`;
+    }
+    console.log({ text });
+    sendNotification(text);
+  } catch (error) {
+    console.log('no new lowest');
   }
-  console.log({ text });
-  sendNotification(text);
 }
-const filePath = join(process.cwd(), 'newLowest.json');
-console.log({ filePath });
-if (existsSync(filePath)) {
-  console.log('send new lowest email');
-  sendNewLowestEmail();
-}
+sendNewLowestEmail();
