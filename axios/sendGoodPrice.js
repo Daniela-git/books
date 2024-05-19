977abec98f4d4ce645390a134c1e4e5e99dfb77a
Daniela <63519335+Daniela-git@users.noreply.github.com>
2024-05-19 10:36:50 -0500

    Update sendGoodPrice.js

--- axios/sendGoodPrice.js
+++ axios/sendGoodPrice.js
@@ -4,7 +4,7 @@ import updateTelegram from './telegramUpdates.js';
 function sendGoodPricesNotification() {
   try {
     const goodPrice = JSON.parse(readFileSync('./goodPrice.json'));
-    let text = 'GOOD PRICE(S)';
+    let text = 'GOOD PRICE(S)/n';
     for (const book of goodPrice) {
       const price = Number(book.price);
       text = `${text}\n${book.title}: $${price.toLocaleString()}`;
